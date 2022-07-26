2f29a0cbd60ea75021825d5e281ac5e6e6e26bf0
Your Name <pjduri@gmail.com>
2022-07-25 22:49:37 -0500

    Studio finished.

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,10 +1,14 @@
 import { Payload } from "./Payload"
+import { Cargo } from "./Cargo"
+import { Astronaut } from "./Astronaut"
 
-export class Rocket {
+export class Rocket implements Payload {
     name: string
     totalCapacityKg: number
-    cargoItems: []
-    astronauts: []
+    cargoItems: Cargo[] = []
+    astronauts: Astronaut[] = []
+    massKg: number
+
     constructor (name: string, totalCapacityKg: number) {
         this.name = name,
         this.totalCapacityKg = totalCapacityKg
@@ -12,10 +16,38 @@ export class Rocket {
 
     sumMass(items: Payload[]): number {
         let mass: number = 0
-        items.forEach((items, index) => {
-            mass += items[index].massKg
-        })
+        for (let i=0; i < items.length; i++) {
+            mass += items[i].massKg
+        }
+        return mass
     }
 
-    
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) 
+        + this.sumMass(this.cargoItems)
+    }
+
+    canAdd(item: Payload): boolean {
+        return (this.currentMassKg() 
+        + item.massKg) <= this.totalCapacityKg
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut)
+            return true
+        } else {
+            return false
+        }
+    }
+
 }
